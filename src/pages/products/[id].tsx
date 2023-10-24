1f87d620047409b029d6b9ed3ce8abb9e6f5e1de
Ariling <97501226+Ariling@users.noreply.github.com>
2023-10-24 22:21:11 +0900

    [test] 변경시에도 deploy잘 되는지 테스트

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -19,6 +19,7 @@ export default function productsPage({ product }: PropductProps) {
   // Render the product
   return (
     <div style={{ height: "100vh", padding: 50 }}>
+      <h1>계속 되는지 테스트...</h1>
       <h1 style={{ color: "red" }}>이름: {product.name}</h1>
       <h2 style={{ color: "blue" }}>가격: {product.price}</h2>
       <p>설명: {product.description}</p>

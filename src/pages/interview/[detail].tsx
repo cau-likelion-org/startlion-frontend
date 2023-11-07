1fb0c4212f75d73747cef79d1c0b6c86a617391c
Ariling <97501226+Ariling@users.noreply.github.com>
2023-11-08 01:32:27 +0900

    [Design] 인터뷰 상세페이지 소개 제외 디자인

--- src/pages/interview/[detail].tsx
+++ src/pages/interview/[detail].tsx
@@ -1,9 +1,24 @@
+import HeightBlank from "@/components/utils/HeightBlank";
 import { useRouter } from "next/router";
 import React from "react";
+import tw from "twin.macro";
+import QNABox from "@/components/interview/QNABox";
 
 const InterviewPage = () => {
   const router = useRouter();
-  return <div>{router.query.detail}인터뷰 관련</div>;
+  return (
+    <div className="flex-col-base py-[120px] w-[1107px] mx-auto">
+      {router.query.detail}인터뷰 관련
+      <QNAWrapper>
+        <QNABox />
+      </QNAWrapper>
+      <HeightBlank />
+    </div>
+  );
 };
 
 export default InterviewPage;
+
+const QNAWrapper = tw.div`
+    w-full gap-[160px] flex flex-col
+`;

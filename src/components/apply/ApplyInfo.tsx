60f63a75f2db9e00555fe92085428b9171605f4e
Ariling <97501226+Ariling@users.noreply.github.com>
2023-11-28 09:58:16 +0900

    [Feat] select창 rough하게 구현

--- src/components/apply/ApplyInfo.tsx
+++ src/components/apply/ApplyInfo.tsx
@@ -1,7 +1,8 @@
 import { ApplyPartAtom } from "@/store/atoms";
-import React from "react";
+import React, { useRef, useState } from "react";
 import { useSetRecoilState } from "recoil";
 import tw, { styled } from "twin.macro";
+import Toggle from "@/svg/toggle.svg";
 
 const ApplyInfo = () => {
   //아직 onClick을 추가안한 상태
@@ -13,6 +14,10 @@ const ApplyInfo = () => {
     "멋쟁이사자처럼 홈페이지",
     "기타",
   ];
+  const [modal, setModal] = useState<Boolean>(false);
+  const modalRef = useRef<HTMLDivElement>(null);
+  const [semester, setSemester] = useState<string>("해당학기");
+
   const setApplyPart = useSetRecoilState(ApplyPartAtom);
   return (
     <>
@@ -58,9 +63,47 @@ const ApplyInfo = () => {
           <ApplyInfoText>다전공</ApplyInfoText>
           <ApplyInput />
         </FlexGap>
-        <div className="gap-6 flex mb-2">
+        <div className="gap-6 flex mb-2 relative">
           <ApplyInfoText>해당 학기*</ApplyInfoText>
-          <ApplyInput />
+          <ApplyInfoSelect
+            onClick={() => {
+              setModal(!modal);
+            }}
+          >
+            <div>
+              {" "}
+              <Toggle
+                style={{
+                  transform: modal ? "rotate(90deg)" : "rotate(0deg)",
+                }}
+              />
+            </div>
+            <div>{semester}</div>
+          </ApplyInfoSelect>
+          {modal ? (
+            <>
+              <MyPostModal ref={modalRef}>
+                <div
+                  onClick={() => {
+                    setModal(!modal);
+                    setSemester("1학기");
+                  }}
+                  className="inner"
+                >
+                  1학기
+                </div>
+                <div
+                  onClick={() => {
+                    setModal(!modal);
+                    setSemester("2학기");
+                  }}
+                  className="inner"
+                >
+                  2학기
+                </div>
+              </MyPostModal>
+            </>
+          ) : null}
         </div>
         <div className="text-[#797979] mb-9 text-right w-full">
           *1학기 기준 해당 학기 선택 바랍니다.
@@ -174,5 +217,54 @@ const ApplyInputRadio = styled.label`
   }
   &.way {
     margin-right: 24px;
+  }
+`;
+
+const ApplyInfoSelect = styled.div`
+  position: relative;
+  width: 126px;
+  height: 42px;
+  box-sizing: border-box;
+  padding: 0px 12px;
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  border-radius: 8px;
+  background-color: #e8eff8;
+  font-size: 20px;
+  line-height: 150%;
+`;
+
+const MyPostModal = styled.div`
+  position: absolute;
+  top: 42px;
+  right: 0px;
+  z-index: 999;
+  width: 126px;
+  height: 90px;
+  border-radius: 6px;
+  border: 1px solid var(--grey-400, #dcdfe3);
+  background-color: var(--white, #fff);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  .inner {
+    width: 120px;
+    height: 42px;
+    border-radius: 4px;
+    color: var(--grey-900, #212224);
+    font-family: Pretendard;
+    font-size: 20px;
+    line-height: 150%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    &:hover {
+      background-color: #e8eff8;
+    }
+    &:active {
+      background-color: #e8eff8;
+    }
   }
 `;

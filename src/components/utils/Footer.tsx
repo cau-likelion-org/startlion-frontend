92f668ebfefd16dc94218e51e3d4830d8ace018b
Ariling <97501226+Ariling@users.noreply.github.com>
2023-12-03 13:40:57 +0900

    [Feat] 링크 연결 및 FAQ 파트 일부 수정

--- src/components/utils/Footer.tsx
+++ src/components/utils/Footer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { styled } from "twin.macro";
-
+import Instagram from "@/svg/footer/insta.svg";
+import MessageSvg from "@/svg/footer/message.svg";
 const FooterBar = styled.div`
   display: flex;
   justify-content: space-around;
@@ -17,18 +18,37 @@ const FooterOptionWrapper = styled.div`
   flex-direction: column;
   justify-content: start;
   gap: 10px;
+  font-weight: 800;
 `;
 
 const Footer = () => {
   return (
     <FooterBar>
       <FooterOptionWrapper>
-        <div>중앙대학교 멋쟁이 사자처럼</div>
-        <div>어쩌구 저쩌구</div>
+        <div className="text-[24px]">중앙대학교 멋쟁이 사자처럼</div>
+        <div className="text-[16px]">어쩌구 저쩌구</div>
       </FooterOptionWrapper>
       <FooterOptionWrapper>
-        <div>CONTACT</div>
-        <button>버튼들</button>
+        <div className="text-[24px]">CONTACT</div>
+        <div className="flex justify-start items-center gap-5">
+          <button
+            onClick={() =>
+              window.open("https://www.instagram.com/likelion_cau/", "_blank")
+            }
+          >
+            <Instagram />
+          </button>
+          <button
+            onClick={() =>
+              window.open(
+                "https://cau-likelion.org/puang@likelion.org",
+                "_blank"
+              )
+            }
+          >
+            <MessageSvg />
+          </button>
+        </div>
       </FooterOptionWrapper>
     </FooterBar>
   );

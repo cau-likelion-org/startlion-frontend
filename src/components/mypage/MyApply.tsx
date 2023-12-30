1452b380d067ba98f1df156c4dcd0e1ddda8a00c
Ariling <97501226+Ariling@users.noreply.github.com>
2023-12-31 02:06:22 +0900

    [Feat] : 마이페이지 및 구글로그인 api 연결 및 그에 따른 코드 수정

--- src/components/mypage/MyApply.tsx
+++ src/components/mypage/MyApply.tsx
@@ -1,9 +1,28 @@
+import { getMypageApi } from "@/apis/mypage/api";
+import { mypageAtom, userInfoState } from "@/store/atoms";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect } from "react";
+import { useRecoilState } from "recoil";
 import tw from "twin.macro";
 
 const MyApply = () => {
   const router = useRouter();
+  const [userToken, setUserToken] = useRecoilState(userInfoState);
+  const [data, setData] = useRecoilState(mypageAtom);
+  useEffect(() => {
+    getMypageApi(userToken.accessToken).then((res) => {
+      if (typeof res === "undefined") {
+        alert("토큰이 유효하지 않습니다. 다시 시도해주세요");
+        setUserToken({
+          accessToken: "",
+          refreshToken: "",
+        });
+        router.replace("/");
+      } else {
+        setData(res);
+      }
+    });
+  }, []);
   return (
     <div className="w-full bg-[#E8EFF8] rounded-[20px] px-8 py-12">
       <TextBar className="mb-[23px] font-bold text-xl pr-[35px]">
@@ -14,6 +33,23 @@ const MyApply = () => {
         <div>지원서</div>
       </TextBar>
       <div className="w-full h-[1px] bg-black"></div>
+      {Array.isArray(data.applicationList) &&
+        data.applicationList.map((e, i) => (
+          <>
+            <TextBar className="mt-[21px] text-base font-normal pr-2">
+              <div>{e.generationId}</div>
+              <div>{e.name}</div>
+              <div>{e.partId}</div>
+              <div>{e.status}</div>
+              <ApplyButton
+                onClick={() => router.push("/apply")}
+                className="ml-[-19px]"
+              >
+                지원서
+              </ApplyButton>
+            </TextBar>
+          </>
+        ))}
       <TextBar className="mt-[21px] text-base font-normal pr-2">
         <div>지원기수</div>
         <div>이름</div>

15b341045ebd465c2964491cb7f09c92a8fc4643
Ariling <97501226+Ariling@users.noreply.github.com>
2024-01-05 08:40:22 +0900

    [!Hotfix] merge하면서 생긴 충돌 해결

--- src/store/testData.tsx
+++ src/store/testData.tsx
@@ -1,120 +1,22 @@
-export interface InterviewCardProp {
-  name: string;
-  part: string;
-  img: string;
-}
-
-export interface InterviewRouteProp extends InterviewCardProp {
-  index: number;
-}
-
-export const InterviewCardTest = [
-  {
-    name: "11기 노지우",
-    part: "디자인",
-    img: "https://cms-cdn.placeholder.co/Vancouver_87c09f1b29.png?width=750",
-    index: 1,
-  },
-  {
-    name: "11기 노지우",
-    part: "디자인",
-    img: "https://cms-cdn.placeholder.co/Vancouver_87c09f1b29.png?width=750",
-    index: 2,
-  },
-  {
-    name: "11기 노지우",
-    part: "디자인",
-    img: "https://cms-cdn.placeholder.co/Vancouver_87c09f1b29.png?width=750",
-    index: 3,
-  },
-  {
-    name: "11기 노지우",
-    part: "디자인",
-    img: "https://cms-cdn.placeholder.co/Vancouver_87c09f1b29.png?width=750",
-    index: 4,
-  },
-  {
-    name: "11기 노지우",
-    part: "디자인",
-    img: "https://cms-cdn.placeholder.co/Vancouver_87c09f1b29.png?width=750",
-    index: 5,
-  },
-  {
-    name: "11기 노지우",
-    part: "디자인",
-    img: "https://cms-cdn.placeholder.co/Vancouver_87c09f1b29.png?width=750",
-    index: 6,
-  },
-  {
-    name: "11기 노지우",
-    part: "디자인",
-    img: "https://cms-cdn.placeholder.co/Vancouver_87c09f1b29.png?width=750",
-    index: 7,
-  },
-  {
-    name: "11기 노지우",
-    part: "디자인",
-    img: "https://cms-cdn.placeholder.co/Vancouver_87c09f1b29.png?width=750",
-    index: 8,
-  },
-  {
-    name: "11기 노지우",
-    part: "디자인",
-    img: "https://cms-cdn.placeholder.co/Vancouver_87c09f1b29.png?width=750",
-    index: 9,
-  },
-  {
-    name: "11기 노지우",
-    part: "디자인",
-    img: "https://cms-cdn.placeholder.co/Vancouver_87c09f1b29.png?width=750",
-    index: 10,
-  },
-  {
-    name: "11기 노지우",
-    part: "디자인",
-    img: "https://cms-cdn.placeholder.co/Vancouver_87c09f1b29.png?width=750",
-    index: 11,
-  },
-  {
-    name: "11기 노지우",
-    part: "디자인",
-    img: "https://cms-cdn.placeholder.co/Vancouver_87c09f1b29.png?width=750",
-    index: 20,
-  },
-  {
-    name: "11기 노지우",
-    part: "디자인",
-    img: "https://cms-cdn.placeholder.co/Vancouver_87c09f1b29.png?width=750",
-    index: 21,
-  },
-  {
-    name: "11기 노지우",
-    part: "디자인",
-    img: "https://cms-cdn.placeholder.co/Vancouver_87c09f1b29.png?width=750",
-    index: 22,
-  },
-  {
-    name: "11기 노지우",
-    part: "디자인",
-    img: "https://cms-cdn.placeholder.co/Vancouver_87c09f1b29.png?width=750",
-    index: 23,
-  },
-  {
-    name: "11기 노지우",
-    part: "디자인",
-    img: "https://cms-cdn.placeholder.co/Vancouver_87c09f1b29.png?width=750",
-    index: 24,
-  },
-  {
-    name: "11기 노지우",
-    part: "디자인",
-    img: "https://cms-cdn.placeholder.co/Vancouver_87c09f1b29.png?width=750",
-    index: 25,
-  },
-  {
-    name: "11기 노지우",
-    part: "디자인",
-    img: "https://cms-cdn.placeholder.co/Vancouver_87c09f1b29.png?width=750",
-    index: 26,
-  },
+export const TotalApplyTimeArray = [
+  "5:00",
+  "6:00",
+  "7:00",
+  "8:00",
+  "9:00",
+  "10:00",
+  "11:00",
+  "12:00",
+  "13:00",
+  "14:00",
+  "15:00",
+  "16:00",
+  "17:00",
+  "18:00",
+  "19:00",
+  "20:00",
+  "21:00",
+  "22:00",
+  "23:00",
+  "24:00",
 ];
